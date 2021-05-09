a87d458211afb0ef9a82d799529f3bf744a3c725
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-10 04:27:39 +0700

    Initial commit

---
+++ app/src/main/java/com/aditya/paypay/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aditya.paypay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aditya.paypay.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
